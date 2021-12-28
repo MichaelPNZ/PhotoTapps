4725f9783e194414b5efdc5117720221b354acd7
MichaelPNZ <85676466+MichaelPNZ@users.noreply.github.com>
2021-12-29 00:02:44 +0300

    add photo

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Михаил Позялов on 28.12.2021.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet var dogImageView: UIImageView!
+}
